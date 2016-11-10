348f75439d65359278548e40d483795e1f9cb304
Robert Nelson <robertcnelson@gmail.com>
2016-11-10 15:51:03 -0600

    gcc6/debian: pass -fno-PIE
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- build.sh
+++ build.sh
@@ -10,6 +10,9 @@ if [ -f .builddir ] ; then
 	fi
 
 	git clone -b ${branch} https://github.com/rcn-ee/dsptop ./src --depth=1
+	cd ./src/
+	patch -p1 < ../../0001-debian-fno-PIE.patch
+	cd ../
 
 	x86_dir="/opt/github/bb.org/ti-4.4/normal"
 	x86_compiler="gcc-linaro-4.9-2016.02-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-"
